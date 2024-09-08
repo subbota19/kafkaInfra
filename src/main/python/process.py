fe0819d5b98e0b4001826d818681952258f96321
yauheni <zhenya.subbota.19@gmail.com>
2024-09-08 22:14:20 +0200

    add parsing, watermark and window

--- src/main/python/process.py
+++ src/main/python/process.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType
-from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StructField, StringType, LongType
+from pyspark.sql.functions import from_json, col, to_timestamp, current_timestamp, window
 
 KAFKA_CONFIG = {
     "kafka.bootstrap.servers": "192.168.49.2:30000",
@@ -8,37 +8,57 @@ KAFKA_CONFIG = {
     "startingOffsets": "earliest"
 }
 
+# local testing
+# .config('spark.jars.packages', 'org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.3')
+# .config('spark.jars.packages', 'org.apache.spark:spark-streaming-kafka-0-10_2.13:3.4.3')
+
 # Initialize SparkSession
 spark = (SparkSession
          .builder
          .appName("KafkaStreamProcessing")
-         # .config('spark.jars.packages', 'org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.3')
-         # .config('spark.jars.packages', 'org.apache.spark:spark-streaming-kafka-0-10_2.12:3.4.3')
          .getOrCreate())
 
 # Read from Kafka as a DataFrame
-kafka_df = spark.readStream.format("kafka").options(**KAFKA_CONFIG).load()
+kafka_df = (spark.readStream
+            .format("kafka")
+            .options(**KAFKA_CONFIG)
+            .load())
 
 schema = StructType([
-    StructField("id", IntegerType()),
+    StructField("id", LongType()),
     StructField("type", StringType()),
     StructField("message", StringType()),
     StructField("datetime", StringType()),
-    StructField("process_id", IntegerType())
+    StructField("process_id", LongType())
 ])
-value_df = (kafka_df
-            .select(from_json(col("value").cast("string"), schema).alias("value"))
-            .selectExpr('value.id',
-                        'value.type',
-                        'value.message',
-                        'value.datetime',
-                        'value.process_id')
-            )
 
-df = (value_df.writeStream
+kafka_df = (kafka_df.
+            withColumn('value', from_json(col('value').
+                                          cast("STRING"), schema)))
+
+kafka_df = (kafka_df
+            .select(
+    col('value.id').alias('id'),
+    col('value.type').alias('type'),
+    col('value.message').alias('message'),
+    col('value.datetime').alias('datetime'),
+    col('value.process_id').alias('process_id'))
+            .withColumn("event_time", to_timestamp(col("datetime")))
+            .withColumn("process_time", current_timestamp())
+
+            .withWatermark('event_time', '10 seconds'))
+
+kafka_df = (kafka_df
+            .groupBy(
+    window(col('event_time'), '1 minute', '1 minute'), col('process_id'))
+            .count().alias('NumberOfProcess')
+            .orderBy(col('NumberOfProcess')))
+
+df = (kafka_df.writeStream
       # .partitionBy("process_id")
       .format("console")
-      .outputMode("append")
+      # .outputMode("append")
+      .outputMode("update")
       .start())
 
 # Start the streaming application to run until the following happens
